e284912f1f520ec424798f83a091c0b18a96639e
kavuturuyeswanth <yeswanth.kavuturu@gmail.com>
2024-06-01 08:49:33 +0200

    Update train.py

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ import torch
 train the agent, the MPI part code is copy from openai baselines(https://github.com/openai/baselines/blob/master/baselines/her)
 
 """
-def get_env_params(env):
-    obs = env.reset()
+def get_env_params(env, seed):
+    obs = env.reset(seed= seed)
     # close the environment
     params = {'obs': obs['observation'].shape[0],
             'goal': obs['desired_goal'].shape[0],
@@ -26,14 +26,15 @@ def launch(args):
     # create the ddpg_agent
     env = gym.make(args.env_name)
     # set random seeds for reproduce
-    env.seed(args.seed + MPI.COMM_WORLD.Get_rank())
+    #env.seed(args.seed + MPI.COMM_WORLD.Get_rank())
     random.seed(args.seed + MPI.COMM_WORLD.Get_rank())
     np.random.seed(args.seed + MPI.COMM_WORLD.Get_rank())
     torch.manual_seed(args.seed + MPI.COMM_WORLD.Get_rank())
     if args.cuda:
         torch.cuda.manual_seed(args.seed + MPI.COMM_WORLD.Get_rank())
     # get the environment parameters
-    env_params = get_env_params(env)
+    env_seed = args.seed + MPI.COMM_WORLD.Get_rank()
+    env_params = get_env_params(env, env_seed)
     # create the ddpg agent to interact with the environment 
     ddpg_trainer = ddpg_agent(args, env, env_params)
     ddpg_trainer.learn()
